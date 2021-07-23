92b8aa9fc43b04f07a1a5a8bd784e4cc7d116cf6
sashabull66 <sashabull9042@gmail.com>
2021-07-23 17:41:50 +0300

    All APP was refactiring with redux

--- src/store/reducers/create.js
+++ src/store/reducers/create.js
@@ -1,21 +1,21 @@
-import {CREATE_QUIZ_QUESTION, RESET_QUIZ_CREATION} from "../actions/actionTypes";
+import {CREATE_QUIZ_QUESTION, RESET_QUIZ_CREATION} from '../actions/actionTypes'
 
-const initialState = { // стартовый state
-    quiz: [],
+const initialState = {
+  quiz: []
 }
 
-export default function createReducer(state = initialState, action) { // создаю reducer для QuizCreator
-    switch (action.type) {
-        case CREATE_QUIZ_QUESTION :
-            return {
-                ...state,
-                quiz: [...state.quiz, action.item]
-            }
-        case RESET_QUIZ_CREATION:
-            return {
-                ...state, quiz: [],
-            }
-        default:
-            return state // если не будет совпадений по action.type
-    }
+export default function createReducer(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_QUIZ_QUESTION:
+      return {
+        ...state,
+        quiz: [...state.quiz, action.item]
+      }
+    case RESET_QUIZ_CREATION:
+      return {
+        ...state, quiz: []
+      }
+    default:
+      return state
+  }
 }
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
-import {combineReducers} from 'redux' // комбаин (сборщик) reducers
-import quizReducer from './quiz' // reducer для QuizList и Quiz
-import createReducer from './create' // reducer для QuizCreator
+import {combineReducers} from 'redux'
+import quizReducer from './quiz'
+import createReducer from './create'
+import authReducer from './auth'
 
-export default combineReducers({ // экспортирую клмбаин и ромещаю в него ссылки на все reducers
-    quiz: quizReducer, // reducer для QuizList и Quiz
-    create: createReducer // reducer для QuizCreator
+export default combineReducers({
+  quiz: quizReducer,
+  create: createReducer,
+  auth: authReducer
 })
